ea02816221f780838ed68ba743bcb0559d98fc29
Kekoa Kim <nalu.kekoa1@gmail.com>
2023-05-16 13:32:22 -0400

    Extra auth

---
+++ api/queries/client.py
@@ -0,0 +1,16 @@
+import os
+from pymongo import MongoClient
+
+MONGO_USER = os.environ.get('MONGO_USER', '')
+MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD', '')
+MONGO_HOST = os.environ.get('MONGO_HOST', '')
+MONGO_DB = os.environ.get('MONGO_DB', '')
+
+client = MongoClient(f'mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}')
+db = client[MONGO_DB]
+db['accounts'].create_index("email", unique=True)
+
+class Queries:
+    @property
+    def collection(self):
+        return db[self.COLLECTION]
