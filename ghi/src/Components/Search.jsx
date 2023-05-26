6cf365a8e226f6156a9b0e828655251ccd9f2e3c
mpreston4 <mick469@gmail.com>
2023-05-26 16:17:52 -0400

    Search By Genre feature added

--- ghi/src/Components/Search.jsx
+++ ghi/src/Components/Search.jsx
@@ -1,22 +1,46 @@
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom'
+import { useGetAllGenresQuery } from "../app/moviesApiSlice";
 
 const Search = () => {
+    const { data } = useGetAllGenresQuery();
     const [title, setTitle] = useState('');
+    const [genre, setGenre] = useState('');
+
     const navigate = useNavigate();
 
-    const handleClick = () => {
+    const handleTitleClick = () => {
         navigate('/movies', {state: {title: title}});
     };
+    const handleGenreClick = () => {
+        navigate('/movies', {state: {genre: genre}});
+    };
+
+
 
     return (
-        <div className="input-group mb-3">
-            <input value={title} onChange={(e) => setTitle(e.target.value)} type="text" className="form-control" placeholder="Search by title..." aria-describedby="basic-addon2"/>
-            <div className="input-group-append">
-                    <button onClick={handleClick} className="btn btn-outline-secondary" type="button">Search</button>
+        <>
+            <div className="input-group mb-3">
+                <input value={title} onChange={(e) => setTitle(e.target.value)} type="text" className="form-control" placeholder="Search by title..." aria-describedby="basic-addon2"/>
+                <div className="input-group-append">
+                    <button onClick={handleTitleClick} className="btn btn-outline-secondary" type="button">Search</button>
+                </div>
             </div>
-        </div>
+
+            <div className="input-group mb-3">
+                <select value={genre} onChange={(e) => setGenre(e.target.value)} className="form-select" placeholder="Search by genre..." aria-describedby="basic-addon2">
+                    <option value="">Choose a Genre</option>
+                    {data.genres.map(genre => {
+                        return (
+                            <option key={genre} value={genre}>{genre}</option>
+                        )
+                    })}
+                </select>
+                <button onClick={handleGenreClick} className="btn btn-outline-secondary" type="button">Search</button>
+            </div>
+        </>
     )
 };
+
 
 export default Search;
