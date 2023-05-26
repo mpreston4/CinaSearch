1ffbdb85dcc93c4fc46bfca04c4e2f58822c5457
Kekoa Kim <nalu.kekoa1@gmail.com>
2023-05-26 14:16:58 -0400

    search fin

--- ghi/src/Components/Search.jsx
+++ ghi/src/Components/Search.jsx
@@ -1,20 +1,19 @@
 import { useState } from "react";
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const Search = () => {
-    const [title, setTitle] = useState('')
-    // const [genre, setGenre] = useState([])
+    const [title, setTitle] = useState('');
+    const navigate = useNavigate();
 
+    const handleClick = () => {
+        navigate('/movies', {state: {title: title}});
+    };
 
     return (
         <div className="input-group mb-3">
             <input value={title} onChange={(e) => setTitle(e.target.value)} type="text" className="form-control" placeholder="Search by title..." aria-describedby="basic-addon2"/>
             <div className="input-group-append">
-                <Link to={{
-                    pathname: "/movies",
-                    state: {search: title}
-                }}
-                className="btn btn-outline-secondary" type="button">Search</Link>
+                    <button onClick={handleClick} className="btn btn-outline-secondary" type="button">Search</button>
             </div>
         </div>
     )
